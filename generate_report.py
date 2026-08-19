c1a234feeccabfd9ebdde6ade648211f1778ea7b
sixline86-ship-it <sixline86@gmail.com>
2026-08-19 16:03:50 +0900

    Update generate_report.py

--- generate_report.py
+++ generate_report.py
@@ -17,7 +17,7 @@ from datetime import datetime
 
 from PIL import Image, ImageDraw, ImageFont
 
-SCRIPT_VERSION = "v2026.08.19-n12"
+SCRIPT_VERSION = "v2026.08.20-o1"
 DATE = datetime.now().strftime("%Y%m%d")
 
 W, H = 1200, 630
