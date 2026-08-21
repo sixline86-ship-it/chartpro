da96119a3055a7beb814616f466898f24e15230c
sixline86-ship-it <sixline86@gmail.com>
2026-08-21 10:57:29 +0900

    Update make_thumb.py

--- make_thumb.py
+++ make_thumb.py
@@ -17,7 +17,7 @@ from datetime import datetime
 
 from PIL import Image, ImageDraw, ImageFont
 
-SCRIPT_VERSION = "v2026.08.21-n21"
+SCRIPT_VERSION = "v2026.08.21-n22"
 DATE = datetime.now().strftime("%Y%m%d")
 
 W, H = 1200, 630
